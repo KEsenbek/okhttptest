52ca471e2ff5acc9d6860f2720b2ba5a2fc7c578
Esenbek Kerimov <esenbekerimov@gmail.com>
2022-08-31 23:45:12 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/okhttptest/controller/AccountController.java
@@ -0,0 +1,54 @@
+package kg.megacom.okhttptest.controller;
+
+import kg.megacom.okhttptest.models.Account;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountController {
+
+    @PostMapping("/save")
+    public Account save(@RequestBody Account account){
+        account.setId(1l);
+        return account;
+    }
+
+    @PutMapping("/update")
+    public Account update(@RequestBody Account account){
+        account.setLogin(account.getLogin() + 234);
+        return account;
+    }
+
+    @GetMapping("/get/{id}")
+    public Account getById(@PathVariable Long id){
+
+        System.out.println("/get/{id}");
+       return Account.builder().
+               id(id)
+               .password("qwerty")
+               .login("admin")
+               .active(true)
+               .build();
+    }
+
+    @DeleteMapping("/delete")
+    public Account delete(@RequestParam Long id){
+        return Account.builder().
+                id(id)
+                .password("qwerty")
+                .login("admin")
+                .active(true)
+                .build();
+    }
+
+    @GetMapping("/get")
+    public Account getById(){
+
+        System.out.println("/get");
+        return Account.builder().
+                id(1l)
+                .password("qwerty")
+                .login("admin")
+                .active(true)
+                .build();
+    }
+}
